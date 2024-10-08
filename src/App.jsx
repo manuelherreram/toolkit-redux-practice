4f8595f8ec379844540a28c08c29a99fbba60655
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-10-07 23:05:46 -0300

    increment and decrement para el counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { increment } from "./store/slices/counter";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./store/slices/counter";
 
 function App() {
   const { counter } = useSelector((state) => state.counter);
@@ -18,10 +22,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <p>count is {counter}</p>
       <div className="card">
-        <button onClick={() => dispatch(increment())}>
-          count is {counter}
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>
+          Increment by 2
         </button>
       </div>
     </>
