1549ad1d0ac45776e019326f46be2bf4f773c918
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-10-09 15:09:53 -0300

    Axios implementado en el thunks

---
+++ src/api/pokemonApi.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const pokemonApi = axios.create({
+  baseUR: "https://pokeapi.co/api/v2",
+});
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page = 0) => {
@@ -5,7 +6,15 @@ export const getPokemons = (page = 0) => {
     dispatch(startLoadingPokemons());
 
     //TODO: realizar peticion http
+    // const resp = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // );
+    // const data = await resp.json();
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
+    );
+    // console.log(resp);
 
-    // dispatch(setPokemons());
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
